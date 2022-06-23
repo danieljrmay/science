57765c55da476956efb6fb6956499c86827cc2ee
Daniel J. R. May <daniel.may@kada-media.com>
2022-06-23 13:08:42 +0100

    Set the default logo

--- template.php
+++ template.php
@@ -10,8 +10,12 @@
  * @see header.tpl.php
  */
 function science_preprocess_header(&$variables) {
+  // Set the theme logo
+  $variables['logo'] = backdrop_get_path('theme', 'science') . 'images/logo.svg';
+
   // Hide the header menu ("My account" and "Log in/out" links) for anonymous
   // users. Admins have to know to navigate to '/user/login'.
   $hide_header_menu = user_is_anonymous() && theme_get_setting('hide_header_menu_for_anonymous', 'science');
   $variables['show_header_menu'] = boolval($variables['menu'] && !$hide_header_menu);
+
 }
